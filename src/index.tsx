d42c8dcf5c8e8eee256dad9ef2a4fceecac47331
Fernando Lopez <fernandojalope343@icloud.com>
2023-12-06 18:10:42 -0500

    exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,32 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import { BiometricItem } from "./components/biometric-item";
+import { BiometricModal } from "./components/biometric-modal";
+import { Devices } from "./components/devices";
+import { FormChangePassword } from "./components/form-change-password";
+import { FormLogin } from "./components/form-login";
+import { FormRecoverPassword } from "./components/form-recover-password";
+import { FormRegister } from "./components/form-register";
+import { Header } from "./components/header";
+import { restaurantCategoriesEs } from "./constants/categories";
+import { ApolloProvider } from "./services/context/apollo.context";
+import { AuthProvider, useAuth } from "./services/context/auth.context";
+import {
+  SessionProvider,
+  useSession,
+} from "./services/context/session.context";
+
+export {
+  ApolloProvider,
+  AuthProvider,
+  FormLogin,
+  FormRegister,
+  Header,
+  SessionProvider,
+  restaurantCategoriesEs,
+  useAuth,
+  useSession,
+  BiometricModal,
+  Devices,
+  FormChangePassword,
+  BiometricItem,
+  FormRecoverPassword,
+};
